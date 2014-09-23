e9bd4929d93a03b21571b9426780177991c013c7
Ryosuke, Nishida <nishida@rurobots.info>
2014-09-23 12:37:30 +0900

    制御系以外のところはだいたいできた

--- rod.cpp
+++ rod.cpp
@@ -12,6 +12,7 @@
 #define dsDrawBox      dsDrawBoxD
 #define dsDrawSphere   dsDrawSphereD
 #define dsDrawCylinder dsDrawCylinderD
+#define dsDrawLine     dsDrawLineD
 #endif
 
 // シミュレーション環境変数
@@ -29,27 +30,27 @@ typedef struct {       // MyObject構造体
 } MyObject;
 MyObject rod[3];          // ロッド（0:SHIELD, 1:ROD, 2:BODY）
 dJointID rod_joint[3];    // ロッドジョイント
-MyObject cannon[3];       // 砲台（0:CANNON, 1:TURRET, 2:BULLET）
-dJointID cannon_joint[3]; // 砲台ジョイント
+MyObject bullet;          // 銃弾オブジェクト
+MyObject ray;             // 光線
 dMatrix3 R;               // 回転行列
 
 
 // オブジェクト定数
 
-#define CANNON_LENGTH 1.0  // 砲台（立方体）一辺（1.0m）
-#define CANNON_WEIGHT 10.0 // 砲台重さ（kg）
+//#define CANNON_LENGTH 1.0  // 砲台（立方体）一辺（1.0m）
+//#define CANNON_WEIGHT 10.0 // 砲台重さ（kg）
 #define CANNON_X 0.0       // 砲台中心座標(X)
 #define CANNON_Y 5.0       // 砲台中心座標(Y)
 #define CANNON_Z 0.5       // 砲台中心座標(Z)
-#define TURRET_LENGTH 1.0  // 砲塔長さ
-#define TURRET_WIDTH 0.25  // 砲塔太さ（正方形）
-#define TURRET_WEIGHT 1.0  // 砲塔重さ
-#define TURRET_X 0.0       // 砲塔中心座標(X)
-#define TURRET_Y 4.5       // 砲塔中心座標(Y)
-#define TURRET_Z 0.5       // 砲塔中心座標(Z)
-#define BULLET_RADIUS 0.105// 弾丸半径（球形：5mm）
-#define BULLET_WEIGHT 0.01 // 弾丸重さ（10g）
-#define ROD_LENGTH 1.4     // ロッドの長さ（1本で両端までの長さ）
+//#define TURRET_LENGTH 1.0  // 砲塔長さ
+//#define TURRET_WIDTH 0.25  // 砲塔太さ（正方形）
+//#define TURRET_WEIGHT 1.0  // 砲塔重さ
+//#define TURRET_X 0.0       // 砲塔中心座標(X)
+//#define TURRET_Y 4.5       // 砲塔中心座標(Y)
+//#define TURRET_Z 0.5       // 砲塔中心座標(Z)
+#define BULLET_RADIUS 0.025// 弾丸半径（球形：25mm）
+#define BULLET_WEIGHT 10.01 // 弾丸重さ（10g）
+#define ROD_LENGTH 1.6     // ロッドの長さ（1本で両端までの長さ）
 #define ROD_WIDTH 0.10     // ロッドの太さ（正方形）
 #define ROD_WEIGHT 1.0     // ロッド重さ（1kg）
 #define SHIELD_RADIUS 0.10 // ロッドベースの半径
@@ -59,7 +60,7 @@ dMatrix3 R;               // 回転行列
 #define SHIELD_Y 0.0       // ロッドベース中心座標(Y)
 #define SHIELD_Z 1.1       // ロッドベース中心座標(Z)
 #define BODY_LENGTH 0.2    // 本体奥行
-#define BODY_WIDTH 1.0     // 本体幅
+#define BODY_WIDTH 0.7     // 本体幅
 #define BODY_HEIGHT 1.8    // 本体高さ
 #define BODY_WEIGHT 70.0   // 本体重さ
 #define BODY_X 0.0         // 本体中心座標(X)
@@ -115,42 +116,14 @@ void create() {
   rod[2].geom = dCreateBox(space, BODY_WIDTH, BODY_LENGTH, BODY_HEIGHT);
   dGeomSetBody(rod[2].geom, rod[2].body);
 
-  // CANNONの生成（空間にz軸回転軸を固定）
-  cannon[0].body = dBodyCreate(world);
-  dBodySetPosition(cannon[0].body, CANNON_X, CANNON_Y, CANNON_Z);
-  dMassSetZero(&mass);
-  dMassSetBoxTotal(&mass, CANNON_WEIGHT, CANNON_LENGTH, CANNON_LENGTH, CANNON_LENGTH);
-  dBodySetMass(cannon[0].body, &mass);
-  cannon[0].geom = dCreateBox(space, CANNON_LENGTH, CANNON_LENGTH, CANNON_LENGTH);
-  dGeomSetBody(cannon[0].geom, cannon[0].body);
-
-  cannon_joint[0] = dJointCreateHinge(world, 0); // ヒンジジョイント
-  dJointAttach(cannon_joint[0], cannon[0].body, 0);
-  dJointSetHingeAnchor(cannon_joint[0], CANNON_X, CANNON_Y, CANNON_Z);
-  dJointSetHingeAxis(cannon_joint[0], 0, 0, 1);// z軸ジョイント
-
-  // TURRETの生成（回転ジョイントx軸に回転軸）
-  cannon[1].body = dBodyCreate(world);
-  dBodySetPosition(cannon[1].body, TURRET_X, TURRET_Y, TURRET_Z);
-  dMassSetZero(&mass);
-  dMassSetBoxTotal(&mass, TURRET_WEIGHT, TURRET_WIDTH, TURRET_LENGTH, TURRET_WIDTH);
-  dBodySetMass(cannon[1].body, &mass);
-  cannon[1].geom = dCreateBox(space, TURRET_WIDTH, TURRET_LENGTH, TURRET_WIDTH);
-  dGeomSetBody(cannon[1].geom, cannon[1].body);
-
-  cannon_joint[1] = dJointCreateHinge(world, 0); // ヒンジジョイント
-  dJointAttach(cannon_joint[1], cannon[1].body, cannon[0].body);
-  dJointSetHingeAnchor(cannon_joint[1], CANNON_X, CANNON_Y, CANNON_Z);
-  dJointSetHingeAxis(cannon_joint[1], 1, 0, 0);// x軸ジョイント
-
   // BULLETの生成（CANNON中心に初期座標）
-  cannon[2].body = dBodyCreate(world);
+  bullet.body = dBodyCreate(world);
   dMassSetZero(&mass);
   dMassSetSphereTotal(&mass, BULLET_WEIGHT, BULLET_RADIUS);
-  dBodySetMass(cannon[2].body,&mass);
-  dBodySetPosition(cannon[2].body, CANNON_X, CANNON_Y, CANNON_Z);
-  cannon[2].geom = dCreateSphere(space, BULLET_RADIUS);
-  dGeomSetBody(cannon[2].geom, cannon[2].body);
+  dBodySetMass(bullet.body,&mass);
+  dBodySetPosition(bullet.body, CANNON_X, CANNON_Y, CANNON_Z);
+  bullet.geom = dCreateSphere(space, BULLET_RADIUS);
+  dGeomSetBody(bullet.geom, bullet.body);
 
 }
 
@@ -183,29 +156,20 @@ static void draw() {
   dsSetColor(0.0 ,0.0 ,1.0);
   dsDrawBox(pos, rot, side);
 
-  // CANNONの描画
-  pos = dBodyGetPosition(cannon[0].body);
-  rot = dBodyGetRotation(cannon[0].body);
-  side[0] = CANNON_LENGTH;
-  side[1] = CANNON_LENGTH;
-  side[2] = CANNON_LENGTH;
-  dsSetColor(0.7 ,0.7 ,0.7);
-  dsDrawBox(pos, rot, side);
-
-  // TURRETの描画
-  pos = dBodyGetPosition(cannon[1].body);
-  rot = dBodyGetRotation(cannon[1].body);
-  side[0] = TURRET_WIDTH;
-  side[1] = TURRET_LENGTH;
-  side[2] = TURRET_WIDTH;
-  dsSetColor(0.7 ,0.7 ,0.7);
-  dsDrawBox(pos, rot, side);
-
   // BULLETの描画
-  pos = dBodyGetPosition(cannon[2].body);
-  rot = dBodyGetRotation(cannon[2].body);
+  pos = dBodyGetPosition(bullet.body);
+  rot = dBodyGetRotation(bullet.body);
   dsSetColor(0 ,0 ,0);
-  dsDrawSphere(pos, rot, BULLET_RADIUS);
+  dsDrawSphere(pos, rot, BULLET_RADIUS+0.1);
+
+  // RAYの描画
+  dVector3 pos_s,pos_e;
+  pos_s[0] = CANNON_X; pos_s[1]=CANNON_Y; pos_s[2]=CANNON_Z;
+  pos_e[0] = (CANNON_Y - BODY_Y) * tan(cannon_q_d[0]);
+  pos_e[1] = BODY_Y;
+  pos_e[2] = CANNON_Z + (CANNON_Y - BODY_Y) * tan(- 1.0 * cannon_q_d[1]) / cos(cannon_q_d[0]);
+  dsSetColor(1 ,0 ,0);
+  dsDrawLine(pos_s, pos_e);
 
 }
 
@@ -217,8 +181,6 @@ static void nearCallback(void *data, dGeomID o1, dGeomID o2) {
   dBodyID b2 = dGeomGetBody(o2);
   if(b1 && b2 && dAreConnected(b1, b2))
     return;
-  //if()
-  //  return;
   const int N = 4;
   dContact contact[N];
   n = dCollide (o1,o2,N,&contact[0].geom,sizeof(dContact));
@@ -246,18 +208,7 @@ static void control() {
   dReal rod_q = dJointGetHingeAngle(rod_joint[1]);
   dReal rod_input = 5.0 * (rod_q_d - rod_q);
   dJointSetHingeParam(rod_joint[1], dParamVel, rod_input);
-  dJointSetHingeParam(rod_joint[1], dParamFMax, 10.0);
-
-  // 砲塔のマニュアル制御
-  for(int i=0; i<2; i++){
-    dReal q = 0.0;
-    dReal cannon_input = 0.0;
-    q = dJointGetHingeAngle(cannon_joint[i]);
-    cannon_input = 5.0 * (cannon_q_d[i] - q);
-    dJointSetHingeParam(cannon_joint[i], dParamVel, cannon_input);
-    dJointSetHingeParam(cannon_joint[i], dParamFMax, 10.0);
-  }
-
+  dJointSetHingeParam(rod_joint[1], dParamFMax, 10000.0);
 }
 
 /*** キー入力関数 ***/
@@ -278,8 +229,10 @@ void command(int cmd)
     break;
   case 'r':
     // CANNONリセット
-    cannon_q_d[0] = 0.0;
-    cannon_q_d[1] = 0.0;
+    {
+      cannon_q_d[0] = 0.0;
+      cannon_q_d[1] = 0.0;
+    }
     break;
   case '[':
     // CANNON回転（水平、左右方向）
@@ -308,10 +261,10 @@ void command(int cmd)
       for(int i=0; i<3; i++){
         cpos[i] += 3*R4[i*4+2];
       }
-      dBodySetPosition(cannon[2].body,cpos[0],cpos[1],cpos[2]);
+      dBodySetPosition(bullet.body,cpos[0],cpos[1],cpos[2]);
       dReal force = 10;
-      dBodySetLinearVel(cannon[2].body,force*R4[2],force*R4[6],force*R4[10]);
-      dBodySetAngularVel(cannon[2].body,0,0,0);
+      dBodySetLinearVel(bullet.body,force*R4[2],force*R4[6],force*R4[10]);
+      dBodySetAngularVel(bullet.body,0,0,0);
       break;
     }
   default:
@@ -326,15 +279,14 @@ static void simLoop(int pause) {
 
   dSpaceCollide(space,0,&nearCallback);  // 衝突検出計算
   dWorldStep(world,0.01);                // 1ステップ進める
-  printf("kokomade---\n");
   dJointGroupEmpty(contactgroup);        // 衝突変数をリセット
 
   draw();  // ロボットの描画
 }
 
 static void start() {
-  static float xyz[3] = { 0.9,0.5,2.0};
-  static float hpr[3] = {-180,0,0};
+  static float xyz[3] = { 0.2,5.2,1.0};
+  static float hpr[3] = { 270,0,0};
   dsSetViewpoint(xyz,hpr);               // 視点，視線の設定
   dsSetSphereQuality(3);                 // 球の品質設定
 }
@@ -355,7 +307,7 @@ int main (int argc, char *argv[]) {
   space        = dHashSpaceCreate(0);
   contactgroup = dJointGroupCreate(0);
 
-  dWorldSetGravity(world,0,0,-9.0);
+  dWorldSetGravity(world,0,0,0);
   dWorldSetERP(world,1.0);          // ERPの設定
   dWorldSetCFM(world,0.0);          // CFMの設定
   ground = dCreatePlane(space,0,0,1,0);
