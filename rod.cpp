239687bcc33b35e75bfbeb5581e21f01f93a72cc
Ryosuke, Nishida <nishida@rurobots.info>
2014-10-04 09:48:30 +0900

    ほぼ完成（メモを見てね）

--- rod.cpp
+++ rod.cpp
@@ -31,7 +31,7 @@ typedef struct {       // MyObject構造体
 MyObject rod[3];          // ロッド（0:SHIELD, 1:ROD, 2:BODY）
 dJointID rod_joint[3];    // ロッドジョイント
 MyObject bullet;          // 銃弾オブジェクト
-MyObject ray;             // 光線
+MyObject target;          // ターゲット
 dMatrix3 R;               // 回転行列
 
 
@@ -39,9 +39,9 @@ dMatrix3 R;               // 回転行列
 
 //#define CANNON_LENGTH 1.0  // 砲台（立方体）一辺（1.0m）
 //#define CANNON_WEIGHT 10.0 // 砲台重さ（kg）
-#define CANNON_X 0.0       // 砲台中心座標(X)
-#define CANNON_Y 5.0       // 砲台中心座標(Y)
-#define CANNON_Z 0.5       // 砲台中心座標(Z)
+#define CANNON_X 10.0       // 砲台中心座標(X)
+#define CANNON_Y 20.0//50.0       // 砲台中心座標(Y)
+#define CANNON_Z 0.6       // 砲台中心座標(Z)
 //#define TURRET_LENGTH 1.0  // 砲塔長さ
 //#define TURRET_WIDTH 0.25  // 砲塔太さ（正方形）
 //#define TURRET_WEIGHT 1.0  // 砲塔重さ
@@ -49,7 +49,7 @@ dMatrix3 R;               // 回転行列
 //#define TURRET_Y 4.5       // 砲塔中心座標(Y)
 //#define TURRET_Z 0.5       // 砲塔中心座標(Z)
 #define BULLET_RADIUS 0.025// 弾丸半径（球形：25mm）
-#define BULLET_WEIGHT 10.01 // 弾丸重さ（10g）
+#define BULLET_WEIGHT 0.01 // 弾丸重さ（10g）
 #define ROD_LENGTH 1.6     // ロッドの長さ（1本で両端までの長さ）
 #define ROD_WIDTH 0.10     // ロッドの太さ（正方形）
 #define ROD_WEIGHT 1.0     // ロッド重さ（1kg）
@@ -71,6 +71,9 @@ dMatrix3 R;               // 回転行列
 
 dReal rod_q_d = 0.0;              // ロッドの目標角度
 dReal cannon_q_d[2] = {0.0, 0.0}; // 砲台の目標角度（0:水平, 1:仰角）
+dReal diff_sum = 0.0;             // 偏差のsum
+dReal mytime = 0.0;               // 制御時間
+bool act_flg = false;              // 追従スイッチ
 
 
 // ロボットの生成
@@ -125,6 +128,15 @@ void create() {
   bullet.geom = dCreateSphere(space, BULLET_RADIUS);
   dGeomSetBody(bullet.geom, bullet.body);
 
+  // TARGETの生成
+//  target.body = dBodyCreate(world);
+//  dMassSetZero(&mass);
+//  dMassSetSphereTotal(&mass, 0.0001, BULLET_RADIUS);
+//  dBodySetMass(target.body,&mass);
+//  dBodySetPosition(target.body, SHIELD_X, SHIELD_Y, SHIELD_Z);
+//  target.geom = dCreateSphere(space, BULLET_RADIUS);
+//  dGeomSetBody(target.geom, target.body);
+
 }
 
 // ロボットの描画
@@ -165,9 +177,9 @@ static void draw() {
   // RAYの描画
   dVector3 pos_s,pos_e;
   pos_s[0] = CANNON_X; pos_s[1]=CANNON_Y; pos_s[2]=CANNON_Z;
-  pos_e[0] = (CANNON_Y - BODY_Y) * tan(cannon_q_d[0]);
-  pos_e[1] = BODY_Y;
-  pos_e[2] = CANNON_Z + (CANNON_Y - BODY_Y) * tan(- 1.0 * cannon_q_d[1]) / cos(cannon_q_d[0]);
+  pos_e[0] = CANNON_X + CANNON_Y * tan(cannon_q_d[0]);
+  pos_e[1] = 0.0;
+  pos_e[2] = CANNON_Z + CANNON_Y * tan(- 1.0 * cannon_q_d[1]) / cos(cannon_q_d[0]);
   dsSetColor(1 ,0 ,0);
   dsDrawLine(pos_s, pos_e);
 
@@ -204,24 +216,96 @@ static void nearCallback(void *data, dGeomID o1, dGeomID o2) {
 
 // 制御式
 static void control() {
-  // ロッドのマニュアル制御
+  mytime += 0.0001;
+/*  // ロッドのマニュアル制御
+
   dReal rod_q = dJointGetHingeAngle(rod_joint[1]);
   dReal rod_input = 5.0 * (rod_q_d - rod_q);
   dJointSetHingeParam(rod_joint[1], dParamVel, rod_input);
   dJointSetHingeParam(rod_joint[1], dParamFMax, 10000.0);
+*/
+  // 弾丸の位置を取得
+  const dReal *pos, *vel;           // 弾丸の位置と速度ベクトル
+  dReal tar[3] = {0.0};             // 計算後の着弾予想点
+  dReal rot[12] = {1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0};
+  //const dReal *tar_pos, *tar_rot;   // 着弾予想地点
+  dReal q_p_d = 0.0;                // 広域レーダーからの目標角度
+  dReal q_c_d = 0.0;                // 近接センサからの偏差
+  dReal distance = 0.0;             // SHIELD中心から弾丸までの距離
+  dReal rod_q = dJointGetHingeAngle(rod_joint[1]);  // RODの現在角度
+  dReal rod_input = 0.0;                            // ROD制御入力
+  pos = dBodyGetPosition(bullet.body);
+  vel = dBodyGetLinearVel(bullet.body);
+  //pos = dBodyGetRelPointPos(bullet.body);
+  //vel = dBodyGetRelPointVel(bullet.body);
+  distance = sqrt(
+    (SHIELD_X-pos[0])*(SHIELD_X-pos[0]) +
+    (SHIELD_Y-pos[1])*(SHIELD_Y-pos[1]) +
+    (SHIELD_Z-pos[2])*(SHIELD_Z-pos[2])
+  );
+
+  if(abs(vel[1])>0.1){
+    tar[0] = pos[0] + pos[1]*vel[0]/abs(vel[1]);
+    tar[1] = 0.0;
+    tar[2] = pos[2] + pos[1]*vel[2]/abs(vel[1]);
+    // 着弾予想点の描画
+    dsSetColor(1 ,0 ,0);
+    dsDrawSphere(tar, rot, BULLET_RADIUS+0.1);
+
+    // 広域レーダー
+    if(distance < 50.0){
+      q_p_d = 1.0 * atan(tar[0]/(tar[2]-SHIELD_Z));
+    }
+
+    // 近接センサ
+    if(distance < 10.0){
+      q_c_d = -1.0 * atan(pos[0]/pos[2]);
+      diff_sum += q_c_d - rod_q;
+    }
+  }
+
+  // 入力トルク（疑似付加目標角度）を計算
+  if(act_flg){  // スイッチがオンの場合
+    rod_input = 500.0 * (q_p_d - rod_q);   // 通常制御のみ
+    //rod_input = 50.0 * (q_p_d - rod_q) + 0.001 * diff_sum;
+    rod_q_d = rod_q;
+    //FILE *fp_0; fp_0 = fopen("dataA.csv","a");
+    //fprintf(fp_0,"%lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf,%3.9lf\n", mytime, pos[0], pos[1], pos[2], vel[0], vel[1], vel[2], tar[0], tar[1], tar[2], q_p_d, q_c_d, rod_q);
+    //fclose(fp_0);
+  }else{
+    rod_input = 500.0 * (rod_q_d - rod_q);   // マニュアル操作
+  }
+
+  //rod_input = 500.0 * (rod_q_d - rod_q);   // マニュアル操作
+  printf("%lf : %lf -> %lf = %lf\r", mytime, rod_q_d, rod_q, rod_input);
+  //printf("%lf, %lf, %lf\r", pos[0], pos[1], pos[2] );
+  //printf("%lf, %lf, %lf\r", vel[0], vel[1], vel[2] );
+  //printf("%lf, %lf, %lf\r", rod_q_d*180.0/M_PI, rod_q*180.0/M_PI, rod_input);
+
+
+  // 制御入力
+  dJointSetHingeParam(rod_joint[1], dParamVel, rod_input);
+  dJointSetHingeParam(rod_joint[1], dParamFMax, 100000.0);
+
 }
 
 /*** キー入力関数 ***/
 void command(int cmd)
 {
   switch(cmd){
+  case '0':
+    if(act_flg)
+      act_flg = false;
+    else
+      act_flg = true;
+    break;
   case 'a':
     // ROD回転（y軸向かって、左方向）
-    rod_q_d += M_PI / 180.0;
+    rod_q_d += 5.0 * M_PI / 180.0;
     break;
   case 's':
     // ROD回転（y軸向かって、右方向）
-    rod_q_d -= M_PI / 180.0;
+    rod_q_d -= 2.0 * M_PI / 180.0;
     break;
   case 'e':
     // RODリセット
@@ -236,23 +320,24 @@ void command(int cmd)
     break;
   case '[':
     // CANNON回転（水平、左右方向）
-    cannon_q_d[0] += M_PI / 180.0;
+    cannon_q_d[0] += 0.101 * M_PI / 180.0;
     break;
   case ']':
     // CANNON回転（水平、左右方向）
-    cannon_q_d[0] -= M_PI / 180.0;
+    cannon_q_d[0] -= 0.101 * M_PI / 180.0;
     break;
   case '1':
     // CANNON回転（仰角、上）
-    cannon_q_d[1] += M_PI / 180.0;
+    cannon_q_d[1] += 0.101 * M_PI / 180.0;
     break;
   case '2':
     // CANNON回転（仰角、下）
-    cannon_q_d[1] -= M_PI / 180.0;
+    cannon_q_d[1] -= 0.101 * M_PI / 180.0;
     break;
   case 'x':
     // CANNON発射
     {
+      mytime = 0.0;
       dMatrix3 R2,R3,R4;
       dRFromAxisAndAngle(R2,0,0,1,cannon_q_d[0]);
       dRFromAxisAndAngle(R3,1,0,0,cannon_q_d[1] + 0.5 * M_PI);
@@ -262,7 +347,7 @@ void command(int cmd)
         cpos[i] += 3*R4[i*4+2];
       }
       dBodySetPosition(bullet.body,cpos[0],cpos[1],cpos[2]);
-      dReal force = 10;
+      dReal force = 500;//500;
       dBodySetLinearVel(bullet.body,force*R4[2],force*R4[6],force*R4[10]);
       dBodySetAngularVel(bullet.body,0,0,0);
       break;
@@ -274,18 +359,16 @@ void command(int cmd)
 
 // シミュレーションループ
 static void simLoop(int pause) {
-
   control();
-
   dSpaceCollide(space,0,&nearCallback);  // 衝突検出計算
-  dWorldStep(world,0.01);                // 1ステップ進める
+  dWorldStep(world,0.0001);                // 1ステップ進める
   dJointGroupEmpty(contactgroup);        // 衝突変数をリセット
-
   draw();  // ロボットの描画
 }
 
 static void start() {
-  static float xyz[3] = { 0.2,5.2,1.0};
+  //static float xyz[3] = { 0.2,5.2,1.0};
+  static float xyz[3] = { 1.0,1.5,1.0};
   static float hpr[3] = { 270,0,0};
   dsSetViewpoint(xyz,hpr);               // 視点，視線の設定
   dsSetSphereQuality(3);                 // 球の品質設定
@@ -312,7 +395,7 @@ int main (int argc, char *argv[]) {
   dWorldSetCFM(world,0.0);          // CFMの設定
   ground = dCreatePlane(space,0,0,1,0);
   create();
-  dsSimulationLoop (argc,argv,320,240,&fn);
+  dsSimulationLoop (argc,argv,500,600,&fn);
   dWorldDestroy (world);
   dCloseODE();
 
